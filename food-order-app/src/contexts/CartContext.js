df364ee9c205e9e7d40e164f84424bdde4a7b545
Katinbox <tranquanki28@gmail.com>
2022-08-07 09:41:45 +0700

    refact: compact code line

--- food-order-app/src/contexts/CartContext.js
+++ food-order-app/src/contexts/CartContext.js
@@ -2,7 +2,6 @@ import { createContext, useContext } from "react";
 
 const CartContext = createContext();
 export const useCartContext = () => {
-   const cartCtx = useContext(CartContext);
-   return cartCtx;
+   return useContext(CartContext);
 };
 export default CartContext;
