c5c8f85fa7701af20301d9b34400fba508601209
katinbox <tranquanki28@gmail.com>
2022-08-24 13:39:57 +0700

    footer

--- expenses_tracker/clients/src/components/MainContent/index.js
+++ expenses_tracker/clients/src/components/MainContent/index.js
@@ -1,6 +1,6 @@
 const MainContent = () => {
   return (
-    <div className="container h-56">
+    <div className="container h-48">
       <h3 className="text-xl text-5D text-center">Spent this week</h3>
       <div className="p-5 text-06 text-6xl text-center flex justify-center">
         <span className="self-start text-3xl text-5D">$</span>
