ef40b294740bc67d3092c563679b14d139642d5c
katinbox <tranquanki28@gmail.com>
2022-10-08 11:09:30 +0700

    feat: move login logic to auth controller

---
+++ expense-web-app/controllers/authController.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+
+const {
+  userController,
+  checkMissing,
+  correctPassword,
+} = require("../controllers/userController");
+const { JWT_SECRET_KEY, JWT_EXPIRES_TIME } = process.env;
+
+module.exports = {
+  login: async (username, password) => {
+    const auth = await authenticate(username, password);
+    if (auth.error) return auth;
+
+    const token = jwt.sign(auth.data.user, JWT_SECRET_KEY, {
+      expiresIn: JWT_EXPIRES_TIME,
+    });
+
+    return { ...auth, token, isAuthenticated: true };
+  },
+};
+
+async function authenticate(username, password) {
+  if (checkMissing(username, password))
+    return message("missing some required keys", true);
+
+  const user = await userController.getByUsername(username);
+
+  if (!user) return message(`${username} not existed`, true);
+
+  if (!correctPassword(password, user.password))
+    return message("username or password incorrect", true);
+
+  return message("Login sucessfully", false, { user });
+}
+
+function message(content = "", error = false, data = {}) {
+  return { msg: content, error, data };
+}
--- expense-web-app/routes/Auth.js
+++ expense-web-app/routes/Auth.js
@@ -1,35 +1,14 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-const {
-  userController,
-  checkMissing,
-  correctPassword,
-} = require("../controllers/userController");
-const { JWT_SECRET_KEY, JWT_EXPIRES_TIME } = process.env;
+const authController = require("../controllers/authController");
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  if (checkMissing(username, password))
-    return res.status(400).json({
-      msg: "Missing some required keys",
-    });
+  const auth = await authController.login(username, password);
+  if (auth.error) return res.status(400).json(auth);
 
-  const user = await userController.getByUsername(username);
-  if (!user) return res.status(400).json({ msg: `${username} not existed` });
-  if (!correctPassword(password, user.password))
-    return res.status(400).json({ msg: "username or password incorrect" });
-
-  const token = jwt.sign(user, JWT_SECRET_KEY, {
-    expiresIn: JWT_EXPIRES_TIME,
-  });
-
-  return res.json({
-    msg: "Login sucessfully",
-    token,
-    isAuthenticated: true,
-  });
+  return res.json(auth);
 });
 
 module.exports = router;
